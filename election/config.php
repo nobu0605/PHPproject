ed21254c56fb37a39d3e7547b5a352edf24867a4
鈴木孝伸 <>
2018-05-16 18:04:16 +0800

    ポートフォリオ

---
+++ election/config.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN', 'mysql:dbname=LAA0951462-9x2l5g;host=mysql128.phy.lolipop.lan');
+define('DB_USERNAME', 'LAA0951462');
+define('DB_PASSWORD', 'YJysCQcQ');
+
+
+session_start();
+
+require_once(__DIR__ . '/functions.php');
---
+++ election/thanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Thanks</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Thank you for your voting！</h1>
+    
+    <a href="index.php"><div id="btn">Go Back</div></a>
+</body>
+</html>
