42b16bd077f226b58e1c6c1fa51fc3a4e706d10b
Nobu <nobublack555@gmail.com>
2018-07-08 15:33:38 +0900

    Update function.php

--- Trabeller/function.php
+++ Trabeller/function.php
@@ -1,8 +1,5 @@
 <?php 
 
-// サインインしているユーザーの情報を取得して、返す関数
-// 引数$db:データベースsつぞくオブジェクト
-// 引数$user_id:サインインしてるユーザーのid 
 function get_signin_user($dbh,$user_id){
     // データベースとの照合処理
     $sql = 'SELECT * FROM `users` WHERE `id`=?';
